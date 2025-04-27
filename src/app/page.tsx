0513a9b68ba0423ff2308c37b20fa7a1198230b5
Jeff Chien <jeffchienmail@gmail.com>
2025-04-27 16:17:40 -0700

    Force dynamic on the index redirect to try to make it go to today Added more romanizations to words.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import timezone from 'dayjs/plugin/timezone';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   redirect("/" + dayjs().tz("Asia/Tokyo").format("YYYY-MM-DD"));
-}
+};
